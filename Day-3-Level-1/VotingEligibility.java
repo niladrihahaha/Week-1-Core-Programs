3cc664521c71b1474d3468ded435bfbd14bcab4f
Niladri <niladri.sen281@gmail.com>
2025-04-29 12:32:54 +0530

    day 3 level 1

---
+++ Day-3-Level-1/VotingEligibility.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] ages = new int[10];
+
+        System.out.println("Enter the age of 10 students:");
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age for student " + (i + 1) + ": ");
+            ages[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 0) {
+                System.out.println("Student " + (i + 1) + " has an invalid age");
+            } else if (ages[i] >= 18) {
+                System.out.println("The student with the age " + ages[i] + " can vote");
+            } else {
+                System.out.println("The student with the age " + ages[i] + " cannot vote");
+            }
+        }
+
+        scanner.close();
+    }
+}
